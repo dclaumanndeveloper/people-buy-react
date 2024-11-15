71a8e2ef22aa39b401ccbb9d69dae27f9e3f9198
Diego <dclaumanndeveloper@gmail.com>
2024-11-15 14:57:36 -0300

    update: add login page button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,14 +4,14 @@ export function Login(){
     return (
         <>
          <div className={styles.page}>
-       <form method="POST" className={styles.formLogin}>
-            <h1>Login</h1>
-            <p>Digite os seus dados de acesso no campo abaixo.</p>
-            <input type="email" placeholder="Digite seu e-mail" />
-            <input type="password" placeholder="Digite seu e-mail" />
-            <a href="/">Esqueci minha senha</a>
-            <input type="submit" value="Acessar" className={styles.btn} />
-        </form>
+            <form method="POST" className={styles.formLogin}>
+                <h1>Login</h1>
+                <p>Digite os seus dados de acesso no campo abaixo.</p>
+                <input type="email" placeholder="Digite seu e-mail" />
+                <input type="password" placeholder="Digite seu e-mail" />
+                <a href="/">Esqueci minha senha</a>
+                <input type="submit" value="Acessar" className={styles.btn} />
+            </form>
         </div>
         </>
     );
