d8e298d5aaa1906fee8cfc4ba1d1834f5b194620
Diego <dclaumanndeveloper@gmail.com>
2024-11-15 17:19:00 -0300

    update: alter login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,9 +7,7 @@ export function Login(){
             <form className={styles.formLogin}>
                 <h1>Bem vindo ao People Buy</h1>
                 <p>Digite os seus dados de acesso no campo abaixo.</p>
-                <label>E-mail:</label>
                 <input type="email" placeholder="Digite seu e-mail" />
-                <label>Password:</label>
                 <input type="password" placeholder="Digite sua senha" />
                 <a href="/">Esqueci minha senha</a>
                 <button type="submit" className={styles.btn}>
